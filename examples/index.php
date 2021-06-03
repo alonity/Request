36d2795f53633f6a0d605c9d0e53bab43999380a
Qexy.org <qexyorg@users.noreply.github.com>
2021-06-03 11:16:03 +0300

    First upload

--- examples/index.php
+++ examples/index.php
@@ -1,13 +1,11 @@
-# Request
-External alonity request
+<?php
 
-## Install
-
-`composer require alonity/request`
-
-### Examples
-```php
 use alonity\request\Request;
+
+ini_set('display_errors', true);
+error_reporting(E_ALL);
+
+require_once('../vendor/autoload.php');
 
 $request = new Request();
 
@@ -17,8 +15,12 @@ $request->setURI($_SERVER['REQUEST_URI']);
 // Send post
 $post = $request->post('https://google.com', ['param1' => 'example', 'param2' => 'test'], ['timeout' => 2]);
 
+var_dump($post);
+
 // Send get
 $post = $request->get('https://google.com', ['param1' => 'example', 'param2' => 'test'], ['timeout' => 1]);
+
+var_dump($post);
 
 // Send multiple
 $post = $request->getStack([
@@ -31,4 +33,7 @@ $post = $request->getStack([
     ['timeout' => 1],
     ['timeout' => 5]
 ]);
-```
+
+var_dump($post);
+
+?>

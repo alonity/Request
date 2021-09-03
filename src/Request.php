5d02afc149a9b9e575c40dd5df0c59cd6ee87a1e
Alonity <alonity@qexy.org>
2021-09-03 14:23:50 +0300

    Add protocol

--- src/Request.php
+++ src/Request.php
@@ -12,7 +12,7 @@
  *
  * @license MIT
  *
- * @version 1.0.0
+ * @version 1.0.1
  *
  */
 
@@ -22,9 +22,9 @@ use alonity\router\RequestInheritance;
 use alonity\router\RequestInterface;
 
 class Request extends RequestInheritance {
-    private $uri;
+    private $uri, $protocol;
 
-    const VERSION = '1.0.0';
+    const VERSION = '1.0.1';
 
     /**
      * Set URI string
@@ -61,6 +61,24 @@ class Request extends RequestInheritance {
      */
     public static function post(string $url, array $data = [], array $options = []) : Handler {
         return new Handler('POST', $url, $data, $options);
+    }
+
+    public function setProtocol(string $protocol) : self {
+        $this->protocol = $protocol;
+
+        return $this;
+    }
+
+    public function getProtocol() : string {
+        if(!is_null($this->protocol)){ return $this->protocol; }
+
+        if(isset($_SERVER['HTTPS']) && filter_var($_SERVER['HTTPS'], FILTER_VALIDATE_BOOLEAN)){ $this->setProtocol('https'); return 'https'; }
+
+        if(isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && strtolower($_SERVER['HTTP_X_FORWARDED_PROTO']) == 'https'){ $this->setProtocol('https'); return 'https'; }
+
+        $this->setProtocol('http');
+
+        return 'http';
     }
 
     /**
